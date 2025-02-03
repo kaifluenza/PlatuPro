d42b129f8697fce9bd10bd9c74a253c46532f387
kai34000 <wanrueni.12@gmail.com>
2025-02-03 12:23:51 -0500

    Impletment delete items function

--- screens/ManageInventoryScreen.js
+++ screens/ManageInventoryScreen.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, SectionList, Button, Modal,TextInput, Pressable} from 'react-native';
+import { View, Text, StyleSheet, SectionList, Button, Modal,TextInput, Alert} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
-import { Swipable } from 'react-native-gesture-handler';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 import inventoryData from '../data/inventoryData';
+
+
 
 const ManageInventoryScreen = () => {
 
@@ -11,7 +13,11 @@ const ManageInventoryScreen = () => {
     const [newItem, setNewItem] = useState("");
     const [selectedCategory, setSelectedCategory] = useState("");
     const [newCategory, setNewCategory] = useState("");
-    
+    const [localCheck, setLocalCheck] = useState(false);
+    const [deleteBtn, setDeleteBtn] = useState(false);
+    let items_to_delete = []; //stores object or object ID?
+    let categories_to_delete = [];
+
     //for managing dropdown picker of categories
     const [open, setOpen] = useState(false);
     const [categories, setCategories] = useState(
@@ -71,19 +77,115 @@ const ManageInventoryScreen = () => {
         setModalVisible(false);
     }
 
+    const handleDelete = () => {
+        //delete items from the data
+        const filteredItems = data.map(section => (
+            { ...section, data:section.data.filter(item => !items_to_delete.includes(item))}
+        ));
 
-    const handleDeleteItem = () => {
+        console.log("after fitlering items: ", filteredItems);
 
+        //then delete categories 
+        const filteredCtgry = filteredItems.filter(section => !categories_to_delete.includes(section.title));
+
+        //update categories in dropdown list too!
+        setCategories([
+            ...filteredCtgry.map((section)=>({label:section.title,value:section.title})),
+            {label:"New Category", value:"New Category"}
+        ]);
+
+        setData(filteredCtgry);
+        items_to_delete=[];
+        categories_to_delete=[];
     }
+
+    const confirmDelete = () => {
+        const categoryText = categories_to_delete.length>0? `Category: ${categories_to_delete.join(", ")}` : "";
+        const itemText = items_to_delete.length>0? `Item: ${items_to_delete.join(", ")}` : "";
+
+        const message = `${categoryText}\n${itemText}`.trim();
+
+        Alert.alert(
+        "Confirm Deletion", 
+        message,
+        [
+            {
+                text: "Cancel", 
+                style: "cancel",
+                onPress: () => {
+                    console.log("user cancel deletion");
+                     //reset delete list to empty
+                     items_to_delete = [];
+                     categories_to_delete = [];
+                },
+            },
+            {
+                text: "Delete",
+                style: "destructive",
+                onPress: () => {
+                    console.log("user confirmed deletion");
+                    handleDelete();
+                },
+            },
+        ])
+    }
+
+    const handleEditItem = () => {
+    }
+
+    const handleEditCategory = () => {
+    }
+
     
     return (
     <View style={styles.container}>
-        <Text style={styles.title}>Manage Inventory</Text>
+        <Text style={styles.title}>Inventory</Text>
         
-        <Button 
-            title="Add New"
-            onPress={()=> setModalVisible(true)}
-        />
+        <View style={styles.horizontalStack}>
+            <Button 
+                title="Add Item"
+                color="white"
+                onPress={()=> setModalVisible(true)}
+            />
+
+            <Button
+                title="Delete"
+                color="#FA302D"
+                onPress={()=> {
+                    if(!deleteBtn){
+                        setDeleteBtn(true)
+                    }else{
+                        if(categories_to_delete.length>0){
+                            console.log("deleting checked categories", categories_to_delete);
+                        }
+
+                        if(items_to_delete.length>0){
+                            console.log("deleting checked items:", items_to_delete);
+                        }
+                        
+                        confirmDelete();
+                        
+                        setDeleteBtn(false); //to hide back the deleting checkboxes
+                    }
+                }}
+             />
+
+             {console.log("delete button state: ", deleteBtn)}
+
+             {deleteBtn && <Button
+                title="Cancel"
+                color="blue"
+                onPress={()=> {
+                    items_to_delete = [];
+                    categories_to_delete = [];
+                    setDeleteBtn(!deleteBtn)
+                    console.log("cancel deletion.");
+                }}
+             />}
+
+
+        </View>
+        
         <Modal 
             animationType='fade'
             transparent={true}
@@ -92,7 +194,7 @@ const ManageInventoryScreen = () => {
         >
             <View style={styles.modalContainer}>
                 <View style={styles.modalContent}>
-                    <Text style={styles.modalText}>Add new item</Text>
+                    <Text style={styles.modalText}>Add New Item</Text>
                     
                     <TextInput
                         style={styles.input}
@@ -120,21 +222,23 @@ const ManageInventoryScreen = () => {
                         value={newCategory}
                         />
                     }
-                    
 
-                    <Button
-                        title="Close"
-                        onPress={()=>{
+                    <View style={styles.horizontalStack}>
+                        <Button
+                         title="Close"
+                         onPress={()=>{
                             setModalVisible(false);
                             setSelectedCategory("");
                             setNewItem("");
-                        }} 
-                    />
+                         }} 
+                        />
 
-                    <Button
-                        title="Add"
-                        onPress={()=> handleAddItem()}
-                    />
+                        <Button
+                         title="Add"
+                         onPress={()=> handleAddItem()}
+                        />
+                    </View>
+                    
                 </View>
             </View>  
         </Modal>
@@ -143,27 +247,63 @@ const ManageInventoryScreen = () => {
             sections={data}
             keyExtractor={(item,index)=>item+index}
             renderItem={({item})=>(
-                <Swipable
-                    renderRightActions={()=>( //defines what will be displayed when user swipe right-to-left
-                       //a red box with a delete button inside
-                        <View style={styles.rightAction}>
-                            <Pressable onPress={()=>handleDeleteItem(item)}>
-                                <Text>Delete</Text>
-                            </Pressable>
-                        </View>
-                    )}
+                <View 
+                    style={styles.item}
+                    flexDirection='row'
+                    justifyContent='space-between'
                 >
+                    <Text style={styles.item}>{item}</Text>
+                    
+                    {deleteBtn && <BouncyCheckbox
+                        isChecked={localCheck}
+                        fillColor='red'
+                        size={20}
+                        onPress={(isChecked)=>{
+                            if(isChecked){
+                                items_to_delete.push(item);
+                            }else{ //user deselect the item
+                                items_to_delete = items_to_delete.filter((arrayItem)=>arrayItem!==item);
+                            }
+                            
+                            console.log("items to be deleted", items_to_delete);
+                        }}
+                        
+                    />}
 
-                    <View style={styles.item}>
-                        <Text>{item}</Text>
-                    </View>
-
-                </Swipable>
+                    
+                     
+                </View>
 
             )} 
 
             renderSectionHeader={({section:{title}})=>(
-                <Text style={styles.sectionHeader}>{title}</Text>
+                <View 
+                    style={styles.sectionHeader}
+                    flexDirection='row'
+                    justifyContent='space-between'
+                >
+                    <Text style={styles.sectionHeader}>{title}</Text>
+                    
+                    {deleteBtn && <BouncyCheckbox
+                        isChecked={localCheck}
+                        fillColor='red'
+                        size={20}
+                        onPress={(isChecked)=>{
+                            if(isChecked){
+                                categories_to_delete.push(title);
+                            }
+                            else{ //user deselect
+                                console.log("user deselected ",title);
+                                categories_to_delete = categories_to_delete.filter((category)=>category!==title);
+                            }
+
+                            console.log("categories to be deleted", categories_to_delete);
+                        }}
+                    />}
+                    
+                    
+                </View>
+                
             )}          
         />
             
@@ -172,6 +312,11 @@ const ManageInventoryScreen = () => {
 };
 
 const styles = StyleSheet.create({
+    horizontalStack:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        
+    },
     container:{
         flex:1,
         padding:24,
@@ -191,17 +336,14 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         backgroundColor: "#FFCB5C",
         borderRadius:8,
-        color: "black",
-        padding:4,
+        padding:5,
     },
     item:{
-        padding: 10,
+        fontSize: 14,
         backgroundColor: "#FF937C",
-        marginVertical: 4,
-        borderRadius: 5,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
+        borderRadius: 8,
+        padding:5,
+        marginVertical:4,
     },
     modalContainer:{
         flex:1,
@@ -212,20 +354,22 @@ const styles = StyleSheet.create({
     modalContent:{
         width:'75%',
         backgroundColor:'#FCD0E9',
-        padding:20,
+        paddingVertical:20,
+        paddingHorizontal:20,
         borderRadius:10,
         alignItems:'center',
+        gap:10,
     },
     modalText:{
         marginBottom:20,
         fontSize:18,
+        fontWeight:500,
     },
     input:{
         width:'100%',
         backgroundColor:"white",
-        padding:6,
+        padding:8,
         borderRadius:4,
-        marginVertical:10,
     },
     picker:{
         width:'100%',
@@ -233,9 +377,6 @@ const styles = StyleSheet.create({
         padding:6,
         borderRadius:4,
         marginVertical:10,
-    },
-    rightAction:{
-        backgroundColor:"red",
     },
     
 });
